a36a65ea6e866e4f662eec9eec57b226c7e5a8aa
Bohdana Ivakhnenko <ivahnenko.bogdana@gmail.com>
2026-01-28 20:53:50 +0100

    Add Cluster intro

---
+++ setup_CLuster.sh
@@ -0,0 +1,13 @@
+# conda remove -n kc --all
+
+conda create --name kc python=3.11.9
+
+conda activate kc
+
+conda install nvidia::cuda==12.6.0
+pip3 install torch torchvision --index-url https://download.pytorch.org/whl/cu126
+
+
+conda install jupyter notebook -c conda-forge
+
+conda install jsonlines transformers
