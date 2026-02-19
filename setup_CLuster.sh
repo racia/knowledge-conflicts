77ccf49f4c221df3409ace8e3eb083251deb817b
Bohdana Ivakhnenko <ivahnenko.bogdana@gmail.com>
2026-02-19 18:59:52 +0100

    Add case studies

--- setup_CLuster.sh
+++ setup_CLuster.sh
@@ -1,13 +1,14 @@
 # conda remove -n kc --all
 
-conda create --name kc python=3.11.9
+conda create --name kc1 python=3.11.9
 
 conda activate kc
 
 conda install nvidia::cuda==12.6.0
 pip3 install torch torchvision --index-url https://download.pytorch.org/whl/cu126
-
+pip3 install accelerate
 
 conda install jupyter notebook -c conda-forge
 
-conda install jsonlines transformers
+conda install jsonlines transformers accelerate
+
