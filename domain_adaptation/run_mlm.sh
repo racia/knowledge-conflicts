f18e506a06875348b3d51650220d743843014274
mario.kuzmanov <mario.kuzmanov@graphwise.ai>
2026-03-17 08:39:38 +0200

    da & eval

---
+++ domain_adaptation/run_mlm.sh
@@ -0,0 +1,24 @@
+CUDA_VISIBLE_DEVICES=0 python3 run_mlm.py \
+    --model_name_or_path google/bigbird-roberta-base \
+    --mlm_probability 0.15 \
+    --train_file dev.csv \
+    --validation_file dev.csv \
+    --output_dir model_hub/bigbird-roberta-base \
+    --learning_rate 5e-5 \
+    --weight_decay 0.01 \
+    --max_grad_norm 1.0 \
+     --max_seq_length 3072 \
+      --num_train_epochs 5 \
+      --dataloader_num_workers 1 \
+       --per_device_train_batch_size 8 \
+       --per_device_eval_batch_size 4 \
+       --gradient_accumulation_steps 2 \
+         --save_steps 1000 \
+         --logging_steps 100 \
+         --seed 42 \
+           --evaluation_strategy "epoch" \
+           --do_train=True \
+            --do_eval=True \
+             --do_predict=True \
+             --report_to "none" \
+             --overwrite_output_dir
