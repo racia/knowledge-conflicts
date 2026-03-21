621e9469b3e12eaf93fb6b484ac6c5cdcd3b6599
Raziye Sari <sari@cl.uni-heidelberg.de>
2026-03-21 10:31:52 +0100

    Improve planting pipeline and refactor

--- utils/model.py
+++ utils/model.py
@@ -38,13 +38,13 @@ def load_model_tokenizer(model_name: str) -> tuple:
     )
     model_kwargs = {
         "device_map": "auto",
-        "torch_dtype": torch.bfloat16,
-        #"quantization_config": quantization_config,
+        "dtype": torch.bfloat16,
+        "quantization_config": quantization_config,
         "attn_implementation": "eager",
-        #"low_cpu_mem_usage": True,
-        #"offload_folder": "offload_folder",
-        #"offload_state_dict": True,
-        #"offload_buffers": True,
+        "low_cpu_mem_usage": True,
+        "offload_folder": "offload_folder",
+        "offload_state_dict": True,
+        "offload_buffers": True,
     }
     tokenizer = AutoTokenizer.from_pretrained(model_name, device_map="auto")
     print("Tokenizer loaded successfully.")
