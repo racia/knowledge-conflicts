1c22bac05c37bdd900e48fd5566082133fb741d9
Raziye Sari <sari@cl.uni-heidelberg.de>
2026-03-21 21:25:31 +0100

    Complete pipeline

--- setup_CLuster.sh
+++ setup_CLuster.sh
@@ -8,7 +8,7 @@ conda activate kc
 conda install nvidia::cuda==12.9 # not necessary if you have the correct Nvidia drivers installed, but it ensures that the correct CUDA version is available in the environment
 pip3 install torch torchvision --index-url https://download.pytorch.org/whl/cu129
 #this version for GTX 1080 Ti Nvidia drivers supports sm_50 sm_60 sm_61 sm_70 sm_75 sm_80 ...
-pip3 install accelerate
+pip3 install accelerate omegaconf
 
 conda install jupyter notebook -c conda-forge
 conda install jsonlines transformers
