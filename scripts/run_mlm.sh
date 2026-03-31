c96d7e200d8e49996229b11682c09c3b5a705098
mario.kuzmanov <mario.kuzmanov@graphwise.ai>
2026-03-31 23:14:43 +0200

    update run_mlm.sh

--- scripts/run_mlm.sh
+++ scripts/run_mlm.sh
@@ -1,23 +1,27 @@
 cd "$HOME/knowledge-conflicts" || exit 1
 
-CUDA_VISIBLE_DEVICES=0 python3 domain_adaptation/run_mlm.py \
+## domain adaptation on conflicting data
+## same goes for baseline data
+## please make sure to adjust the paths
+
+CUDA_VISIBLE_DEVICES=0 python3 run_mlm.py \
     --model_name_or_path google/bigbird-roberta-base \
     --mlm_probability 0.15 \
-    --train_file train-conflicts.csv \
-    --validation_file dev.csv \
-    --output_dir model_hub/bigbird-roberta-base-conflicted \
-    --learning_rate 2e-5 \
+    --train_file new_data/train-conflicts.csv \
+    --validation_file new_data/dev.csv \
+    --output_dir model_hub/bigbird-roberta-base-conflicts \
+    --learning_rate 3e-5 \
     --warmup_ratio 0.05 \
     --weight_decay 0.01 \
     --max_grad_norm 1.0 \
-     --max_seq_length 3072 \
-      --num_train_epochs 5 \
+     --max_seq_length 2048 \
+      --num_train_epochs 8 \
       --dataloader_num_workers 4 \
        --per_device_train_batch_size 4 \
        --per_device_eval_batch_size 2 \
        --gradient_accumulation_steps 2 \
          --save_strategy "epoch" \
-         --logging_steps 500 \
+         --logging_steps 200 \
          --seed 42 \
            --evaluation_strategy "epoch" \
            --do_train=True \
